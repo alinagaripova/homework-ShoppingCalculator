8f9570185b5d019a18852c796a75083b1a5ddfe2
alinagaripova <alinagaripova25@yandex.ru>
2019-02-20 17:49:43 +0300

    added storage.js

---
+++ src/js/storage.js
@@ -0,0 +1,80 @@
+export class Product {
+    constructor(name, price) {
+        this.name = name;
+        this.price = price;
+    }
+}
+
+export class AmountLocalStorage {
+    constructor() {
+        const savedItems = JSON.parse(localStorage.getItem('products'));
+        if (savedItems !== null) {
+            this.items = savedItems;
+        } else {
+            this.items = [];
+        }
+    }
+
+    add(item) {
+        this.items.push(item);
+        this.save();
+    }
+
+    sum() {
+        let result = 0;
+        for (const item of this.items) {
+            result = result + item.price;
+        }
+        this.save();
+        return result;
+
+    }
+
+    findMoreExpensiveProduct() {
+        let price = this.items[0].price;
+        let name = this.items[0].name;
+        for (const item of this.items) {
+            if (price < item.price) {
+                price = item.price;
+                name = item.name;
+            }
+        }
+        this.save();
+        return name + ' ' + price + ' руб.';
+    }
+
+    save() {
+        localStorage.setItem('products', JSON.stringify(this.items));
+    }
+}
+
+
+export class AmountInMemoryStorage {
+    constructor() {
+        this.items = []
+    }
+
+    add(item) {
+        this.items.push(item); //количество покупок
+    }
+
+    sum() { //общая стоимость покупок
+        let result = 0;
+        for (const item of this.items) {
+            result = result + item.price;
+        }
+        return result;
+    }
+
+    findMoreExpensiveProduct() { //самая дорогая покупка
+        let price = this.items[0].price;
+        let name = this.items[0].name;
+        for (const item of this.items) {
+            if (price < item.price) {
+                price = item.price;
+                name = item.name;
+            }
+        }
+        return name + ' ' + price + ' руб.';
+    }
+}
