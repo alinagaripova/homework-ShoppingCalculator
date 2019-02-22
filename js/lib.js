5140fd1f74b2c57b1bbbeb78fb6b941236061fe3
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-02-22 11:28:28 +0000

    Deploy alinagaripova/homework-ShoppingCalculator to github.com/alinagaripova/homework-ShoppingCalculator.git:gh-pages

--- js/lib.js
+++ js/lib.js
@@ -1,29 +1,29 @@
-export class Product {
-    constructor(name, price) {
-        this.name = name;
-        this.price = price;
-    }
-}
-
-export class Amount {
-    constructor(storage) {
-        this.storage = storage;
-    }
-
-    get items() {
-        return this.storage.items;
-    }
-
-    add(item) { //количество покупок
-        this.storage.add(item);
-    }
-
-    sum(item) { //общая стоимость покупок
-        this.storage.sum(item);
-    }
-
-    findMoreExpensiveProduct(item) { //самая дорогая покупка
-        this.storage.findMoreExpensiveProduct(item);
-    }
-}
-
+// export class Product {
+//     constructor(name, price) {
+//         this.name = name;
+//         this.price = price;
+//     }
+// }
+//
+// export class Amount {
+//     constructor(storage) {
+//         this.storage = storage;
+//     }
+//
+//     get items() {
+//         return this.storage.items;
+//     }
+//
+//     add(item) { //количество покупок
+//         this.storage.add(item);
+//     }
+//
+//     sum(item) { //общая стоимость покупок
+//         this.storage.sum(item);
+//     }
+//
+//     findMoreExpensiveProduct(item) { //самая дорогая покупка
+//         this.storage.findMoreExpensiveProduct(item);
+//     }
+// }
+//
--- js/storage.js
+++ js/storage.js
@@ -51,34 +51,3 @@ export class AmountLocalStorage {
         localStorage.setItem('products', JSON.stringify(this.items));
     }
 }
-
-
-export class AmountInMemoryStorage {
-    constructor() {
-        this.items = []
-    }
-
-    add(item) {
-        this.items.push(item); //количество покупок
-    }
-
-    sum() { //общая стоимость покупок
-        let result = 0;
-        for (const item of this.items) {
-            result = result + item.price;
-        }
-        return result;
-    }
-
-    findMoreExpensiveProduct() { //самая дорогая покупка
-        let price = this.items[0].price;
-        let name = this.items[0].name;
-        for (const item of this.items) {
-            if (price < item.price) {
-                price = item.price;
-                name = item.name;
-            }
-        }
-        return name + ' ' + price + ' руб.';
-    }
-}
